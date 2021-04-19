c32bcfdbb55bad08750538b639121a34e38ddc29
Bakyt <djumabaevb@gmail.com>
2021-04-19 17:03:33 +0600

    Initial commit

---
+++ app/src/main/java/com/example/frameanimation/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.frameanimation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private AnimationDrawable batAnimation;
+    private ImageView batImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        batImage = (ImageView) findViewById(R.id.bat_id);
+        batImage.setBackgroundResource(R.drawable.bat_anim);
+    }
+}
